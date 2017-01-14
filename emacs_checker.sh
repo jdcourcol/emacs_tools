813e41bd460037ec19823907e5b186ae3ab74f1c
jean-denis courcol <jean-denis.courcol@epfl.ch>
2017-01-14 17:05:16 +0100

    fixed pep8 and removed traces

--- emacs_checker.sh
+++ emacs_checker.sh
@@ -2,7 +2,6 @@
 SCRIPT=$(greadlink -f "$0")
 SCRIPTPATH=`dirname ${SCRIPT}`
 echo "script path" $SCRIPTPATH > /tmp/echo.txt
-env > /tmp/env.txt
-${SCRIPTPATH}/epylint.py "$1" ${SCRIPTPATH}/pylintrc $VIRTUAL_ENV 2>/tmp/echo2.txt
-pep8 --config=${SCRIPTPATH}/pep8rc --repeat "$1"
+${SCRIPTPATH}/epylint.py "$1" ${SCRIPTPATH}/pylintrc $VIRTUAL_ENV 2>/dev/null
+$VIRTUAL_ENV/bin/pep8 --config=${SCRIPTPATH}/pep8rc --repeat "$1"
 true
